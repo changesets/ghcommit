b0c7f0d31891fb998a77c49cf1f6a4a8d09404ee
bluwy <bjornlu.dev@gmail.com>
2026-06-11 17:02:47 +0800

    Update vitest config

--- vitest.integration.config.ts
+++ vitest.integration.config.ts
@@ -16,6 +16,8 @@ process.env.ROOT_TEMP_DIRECTORY ??= path.join(
 
 export default defineConfig({
   test: {
+    experimental: { preParse: true },
+    clearMocks: true,
     globalSetup: ["./tests/integration/globalSetup.ts"],
     include: ["tests/integration/**/*.test.ts"],
   },
