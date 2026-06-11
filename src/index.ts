9ac746d10c551f0e167aa88e87346378860a8270
Bjorn Lu <bjornlu.dev@gmail.com>
2026-06-11 14:42:27 +0800

    Remove queries export (#91)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-export * as queries from "./github/graphql/queries.ts";
 export { commitFilesFromBase64 } from "./core.ts";
 export { commitChangesFromRepo } from "./git.ts";
 export { commitFilesFromDirectory } from "./fs.ts";
