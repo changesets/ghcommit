a24e1f3a0e99ee8cec026d1282a760b100e3c916
Bjorn Lu <bjornlu.dev@gmail.com>
2026-06-16 18:00:57 +0800

    Apply suggestions from code review
    Co-authored-by: Bjorn Lu <bjornlu.dev@gmail.com>

--- src/index-browser.ts
+++ src/index-browser.ts
@@ -1,5 +1,7 @@
 export { commitFilesFromBase64 } from "./core.ts";
 
 export function commitChangesFromRepo() {
-  throw new Error("commitChangesFromRepo is not supported in the browser");
+  throw new Error(
+    "commitChangesFromRepo is not supported in non-Node.js environments"
+  );
 }
